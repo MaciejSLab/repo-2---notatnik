a49786deb78e7c8d79f6ff15f5c29b39202372a1
Kamil Dróżdż <kamil12x@wp.pl>
2023-12-12 22:08:27 +0100

    fix: corrections to the todo.js code so that the todo functionality is fully functional, you can add, remove, mark completed

--- todo.js
+++ todo.js
@@ -1,18 +1,40 @@
 function addTask() {
   const input = document.querySelector("#task");
-
   const taskText = input.value.trim();
   if (taskText) {
-    document
-      .querySelector("#task-list")
-      createElementTask(taskText, false));
+    const taskList = document.querySelector("#task-list");
+    const taskItem = createElementTask(taskText, false);
+    taskList.appendChild(taskItem);
+    input.value = "";
   }
 }
 
 function createElementTask(taskText, isCompleted) {
-  const textDecoration = isCompleted ? "line-through" : "none";
-  const span = document.createElement("span");
-  span.textContent = taskText;
   const li = document.createElement("li");
+
+  const span = document.createElement("span");
+  span.style.textDecoration = isCompleted ? "line-through" : "none";
+  span.textContent = taskText;
   li.appendChild(span);
+
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "Usuń";
+  removeButton.addEventListener("click", function () {
+    li.remove();
+  });
+
+  const toggleButton = document.createElement("button");
+  toggleButton.textContent = "Wykonane";
+  toggleButton.addEventListener("click", function () {
+    if (span.style.textDecoration === "line-through") {
+      span.style.textDecoration = "none";
+    } else {
+      span.style.textDecoration = "line-through";
+    }
+  });
+
+  li.appendChild(removeButton);
+  li.appendChild(toggleButton);
+
+  return li;
 }
